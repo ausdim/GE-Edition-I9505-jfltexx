f90c88cc9f439e2933ffdec4ed96f91fa76ade7a
ktoonsez <ktoonsez@gmail.com>
2013-05-30 16:33:50 -0700

    mDNIE: Fix Samsung typos for typedefs on mode and scenario names

--- drivers/video/msm/mdnie_lite_tuning.h
+++ drivers/video/msm/mdnie_lite_tuning.h
@@ -92,9 +92,9 @@ enum Lcd_mDNIe_Negative {
 };
 
 enum Background_Mode {
-	DYNAMIC_MODE = 0,
-	STANDARD_MODE,
-#if !defined(CONFIG_SUPPORT_DISPLAY_OCTA_TFT)
+	STANDARD_MODE = 0,
+	DYNAMIC_MODE,
+#if !defined(CONFIG_SUPPORT_DISPLAY_OCTA_TFT)	
 	NATURAL_MODE,
 #endif
 	MOVIE_MODE,
