85c6a26b4ac86cfd03eaabe32f0b9efffcf86dee
ausdim <koronaios@gmail.com>
2013-10-03 12:48:16 +0300

    Zram disk's = 4

--- drivers/staging/zram/zram_drv.c
+++ drivers/staging/zram/zram_drv.c
@@ -40,7 +40,7 @@ static int zram_major;
 struct zram *zram_devices;
 
 /* Module params (documentation at end) */
-static unsigned int num_devices;
+static unsigned int num_devices = 4;
 
 static void zram_stat_inc(u32 *v)
 {
@@ -802,13 +802,7 @@ static int __init zram_init(void)
 		goto out;
 	}
 
-	if (!num_devices) {
-		pr_info("num_devices not specified. Using default: 1\n");
-		num_devices = 1;
-	}
-
 	/* Allocate the device array and initialize each one */
-	pr_info("Creating %u devices ...\n", num_devices);
 	zram_devices = kzalloc(num_devices * sizeof(struct zram), GFP_KERNEL);
 	if (!zram_devices) {
 		ret = -ENOMEM;
@@ -820,6 +814,7 @@ static int __init zram_init(void)
 		if (ret)
 			goto free_devices;
 	}
+		pr_info("Created %u device(s) ...\n", num_devices); 
 
 	return 0;
 
